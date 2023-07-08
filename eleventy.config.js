3f50b6fbcd90c2a503ee8ab7b1b1f3d3e6df6c56
mitorun-studio <mitorun.studio@gmail.com>
2023-07-08 16:46:46 +0700

    ADD sharp and typograf

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,15 +1,14 @@
 const htmlMinifierTerser = require('html-minifier-terser');
-const lightningCSS = require("@11tyrocks/eleventy-plugin-lightningcss");
+const lightningCSS = require('@11tyrocks/eleventy-plugin-lightningcss');
 // const esbuild = require('esbuild');
-const pluginBundle = require("@11ty/eleventy-plugin-bundle");
-const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const typograf = require('typograf');
-// const markdownIt = require('markdown-it')({ html: true });
+const pluginBundle = require('@11ty/eleventy-plugin-bundle');
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+const markdownIt = require('markdown-it')({ html: true });
 
 
 
 
-module.exports = function (eleventyConfig) {
+module.exports = function(eleventyConfig) {
 
 	//------------------------------------------------
 	// Настройки Eleventy: ---------------------------
@@ -17,28 +16,28 @@ module.exports = function (eleventyConfig) {
 
 	// Настройка параметров сервера: -----------------
 	eleventyConfig.setServerOptions({
-		//watch: ["site/**/*.css"],
+		//watch: ['site/**/*.css'],
 		showAllHosts: true, // Показать локальные IP-адреса сети для тестирования устройств.
 	});
 	// Задержка в отслеживании (watch):
 	eleventyConfig.setWatchThrottleWaitTime(1000); // 0ms default.
 
 	// Отслеживание (watch) картинок для конвейера (pipeline):
-	//eleventyConfig.addWatchTarget("img/**/*.{svg,avif,webp,png,jpg,jpeg}");
+	//eleventyConfig.addWatchTarget('img/**/*.{svg,avif,webp,png,jpg,jpeg}');
 
 
 	// Плагины: --------------------------------------
 
 	eleventyConfig.addPlugin(pluginBundle, {
 		// Папка в выходном каталоге куда будут записываться файлы пакетов:
-		toFileDirectory: "bundle",
-		// Дополнительные имена бандлов ("css", "js", "html" есть по умолчанию):
+		toFileDirectory: 'bundle',
+		// Дополнительные имена бандлов ('css', 'js', 'html' есть по умолчанию):
 		bundles: [],
 		// Array of async-friendly callbacks to transform bundle content.
 		// Работают с getBundle и getBundleFileUrl:
 		transforms: [],
 		// Array of bundle names eligible for duplicate bundle hoisting
-		hoistDuplicateBundlesFor: [], // e.g. ["css", "js"]
+		hoistDuplicateBundlesFor: [], // e.g. ['css', 'js']
 	});
 
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -52,13 +51,26 @@ module.exports = function (eleventyConfig) {
 
 	// Фильтр для удобочитаемой русской даты:
 	// Использование: "{{ date | readableDate }}", результат: "13 ноября 2022":
-	eleventyConfig.addFilter("readableDate", function (value) {
-		return value.toLocaleString("ru", {
-			day: "numeric",
-			month: "long", // long | short.
-			year: "numeric"
-		}).replace(" г.", "");
+	eleventyConfig.addFilter('readableDate', function(value) {
+		return value.toLocaleString('ru', {
+			day: 'numeric',
+			month: 'long', // long | short.
+			year: 'numeric'
+		}).replace(' г.', '').replace('г.', '');
 	});
+
+	// Добавление функционала маркдауна {{ content | markdown | safe }}
+	eleventyConfig.addFilter('markdown', function(value) {
+		return markdownIt.render(value);
+	});
+
+	// Добавление функционала маркдауна инлайн {{ post.title | markdownInline | safe }}
+	eleventyConfig.addFilter('markdownInline', function(value) {
+		return markdownIt.renderInline(value);
+	});
+
+	eleventyConfig.setLibrary('md', markdownIt);
+
 
 
 
@@ -67,11 +79,11 @@ module.exports = function (eleventyConfig) {
 	//------------------------------------------------
 
 	// Добавить текущий год {% year %}:
-	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+	eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
-	// Генерация OpenGraph картинки для каждой страницы:
-	eleventyConfig.addShortcode("opengraph", function (baseUrl) {
-		const pathPrefix = eleventyConfig.pathPrefix || "";
+	// Генерация картинки OpenGraph для каждой страницы {% opengraph _site.siteUrl %}:
+	eleventyConfig.addShortcode('encodeuri', function (baseUrl) {
+		const pathPrefix = eleventyConfig.pathPrefix || '';
 		const encodedURL = encodeURIComponent(baseUrl + pathPrefix + this.page.url);
 		return encodedURL;
 	});
@@ -80,60 +92,6 @@ module.exports = function (eleventyConfig) {
 	//------------------------------------------------
 	// Обработка HTML: -------------------------------
 	//------------------------------------------------
-
-
-	// Больше правил можно найти здесь: https://github.com/typograf/typograf/blob/dev/docs/RULES.ru.md
-	// Создаём фильтр для шаблонов {{ content | typograf | safe }}:
-	//eleventyConfig.addFilter('typograf', function(content) {
-	//	return typograf.execute(content);
-	//});
-
-	eleventyConfig.addTransform('typograf', function (content, path) {
-		if (path.endsWith('.html') || path.endsWith('.md')) {
-				const tp = new typograf({
-					locale: ['ru', 'en-US'],
-					safeTags: [
-						['<\\?php', '\\?>'],
-						['<no-typography>', '</no-typography>']
-					],
-					disableRule: [
-						//'common/nbsp/afterShortWord',// Неразрывный пробел после короткого слова (№12)
-						'common/number/fraction',// 1/2 → ½, 1/4 → ¼, 3/4 → ¾ (№19)
-						'common/number/mathSigns',// != → ≠, <= → ≤, >= → ≥, ~= → ≅, +- → ± (№20)
-						//'common/number/times',// x → × (10 x 5 → 10×5) (№21)
-						//'common/punctuation/hellip	',// Замена трёх точек на многоточие (№26)
-						'common/punctuation/quote',// Расстановка кавычек правильного вида (№27)
-						'common/space/replaceTab',// Замена таба на 4 пробела (№45)
-						'common/symbols/cf',// Добавление ° к C и F (№50)
-						'en-US/dash/main',// Замена дефиса на длинное тире (№52)
-						'ru/dash/main',// Замена дефиса на тире (№63)
-						'ru/dash/surname',// Сокращения с помощью тире (№65)
-						'ru/date/fromISO',// Преобразование дат YYYY-MM-DD к виду DD.MM.YYYY (№71)
-						'ru/number/comma',// Замена точки на запятую в числах (№91)
-						//'ru/other/phone-number',// Форматирование телефонных номеров "+7 495 123-45-67" (№97)
-						'ru/punctuation/exclamation',// !! → ! (№99)
-						'ru/punctuation/exclamationQuestion',// !? → ?! (№100)
-						'ru/punctuation/hellipQuestion',// «?…» → «?..», «!…» → «!..», «…,» → «…» (№101)
-						'ru/typo/switchingKeyboardLayout'// Замена латинских букв на русские. Опечатки, возникающие при переключении клавиатурной раскладки (№105)
-					],
-					enableRule: [
-						//'common/html/stripTags',// Удаление HTML-тегов (№7)
-						//'common/nbsp/afterNumber',// Неразрывный пробел между числом и словом (№9)
-						//'common/nbsp/replaceNbsp',// Замена неразрывного пробела на обычный перед типографированием (№17)
-						//'common/number/digitGrouping',// Разбивать длинные числа по разрядам (№18)
-						//'ru/optalign/bracket',// для открывающей скобки (№93)
-						//'ru/optalign/comma',// для запятой (№94)
-						//'ru/optalign/quote',// для открывающей кавычки (№95)
-						//'ru/other/accent',// Замена заглавной буквы на строчную с добавлением ударения (№96)
-					]
-				});
-				return tp.execute(content);
-		}
-		return content;
-	});
-
-
-
 
 	// Добавление преобразования для минимизации HTML-файлов:
 	eleventyConfig.addTransform('html-minify', function (content, path) {
@@ -172,7 +130,7 @@ module.exports = function (eleventyConfig) {
 
 	// Добавление плагина LightningCSS:
 	eleventyConfig.addPlugin(lightningCSS, {
-		//importPrefix: "_", // Тип: строка, по умолчанию: "_".
+		//importPrefix: '_', // Тип: строка, по умолчанию: '_'.
 		//nesting: true, // Тип: логический, по умолчанию: true.
 		//customMedia: true, // Тип: логический, по умолчанию: true.
 		//minify: true, // Тип: логический, по умолчанию: true.
@@ -223,14 +181,14 @@ module.exports = function (eleventyConfig) {
 	// Прямое копирование файлов и папок: ------------
 	//------------------------------------------------
 
-	eleventyConfig.addPassthroughCopy("src/img");
-	//eleventyConfig.addPassthroughCopy("src/img/**/*.{svg,avif,webp,jxl,jpg,jpeg,png,tif,tiff,bmp,gif}");
-	eleventyConfig.addPassthroughCopy("src/fls");
-	eleventyConfig.addPassthroughCopy("src/*.{js,php,txt,xml,json,webmanifest,htaccess,ico}");
+	eleventyConfig.addPassthroughCopy('src/img');
+	//eleventyConfig.addPassthroughCopy('src/img/**/*.{svg,avif,webp,jxl,jpg,jpeg,png,tif,tiff,bmp,gif}');
+	eleventyConfig.addPassthroughCopy('src/fls');
+	eleventyConfig.addPassthroughCopy('src/*.{js,php,txt,xml,json,webmanifest,htaccess,svg,ico}');
 
 	//eleventyConfig.addPassthroughCopy({
-	//	"./public/": "/",
-	//	"./node_modules/prismjs/themes/prism-okaidia.css": "/css/prism-okaidia.css"
+	//	'./public/': '/',
+	//	'./node_modules/prismjs/themes/prism-okaidia.css': '/css/prism-okaidia.css'
 	//});
 
 
@@ -241,18 +199,18 @@ module.exports = function (eleventyConfig) {
 	//------------------------------------------------
 
 	return {
-		pathPrefix: "/kirshmelev.ru/", // Указываем имя репозитория на Github (можно оставить пустым, если не используется).
+		pathPrefix: '/kirshmelev.ru/', // Можно указать дополнение к ссылкам и использовать через фильтр "| url", например имя репозитория на Github (но для этого лучше использовать плагин html-base).
 		addPassthroughFileCopy: true,
-		dataTemplateEngine: "njk",
-		markdownTemplateEngine: "njk",
-		htmlTemplateEngine: "njk",
-		templateFormats: ["html", "njk", "md"],
+		dataTemplateEngine: 'njk',
+		markdownTemplateEngine: 'njk',
+		htmlTemplateEngine: 'njk',
+		templateFormats: ['html', 'njk', 'md'],
 		dir: {
-			input: "src",
-			output: "site",
-			includes: "_includes",
-			layouts: "_includes",
-			data: "_includes"
+			input: 'src',
+			output: 'site',
+			includes: '_includes',
+			layouts: '_includes',
+			data: '_includes'
 		}
 	};
 };
